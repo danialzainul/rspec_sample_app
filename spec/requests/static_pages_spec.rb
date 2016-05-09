749e9d99b4ce4b4ac3366ec98b334d13e1319b6a
Danial <danialzainul@hotmail.com>
2016-05-09 11:53:54 +0800

    added more testing for homepage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,20 +18,29 @@ describe "Static pages" do
 		before { visit help_path }
 
 		it { should have_content('Help') }
-		it { should have_title(full_title('')) }
+		it { should have_title(full_title('Help')) }
 	end
 
 	describe "About page" do
 		before { visit about_path }
 
 		it { should have_content('About Us') }
-		it { should have_title(full_title('')) }
+		it { should have_title(full_title('About Us')) }
 	end
 
 	describe "Contact page" do
 		before { visit contact_path }
 
-		it { should have_content 'Contact' }
-		it { should have_title full_title('') }
+		it { should have_selector('h1', text: 'Contact') }
+		it { should have_title(full_title('Contact')) }
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link 'About'; expect(page).to have_title(full_title('About Us'))
+		click_link 'Contact'; expect(page).to have_title(full_title('Contact'))
+		click_link 'Help'; expect(page).to have_title(full_title('Help'))
+		click_link 'sample app'; expect(page).to have_title(full_title(''))
+		visit root_path; click_link "Sign up now!"; expect(page).to have_title(full_title('Signup'))
 	end
 end
