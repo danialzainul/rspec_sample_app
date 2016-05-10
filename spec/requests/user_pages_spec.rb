68810f5f7f7ea38233796aa1a485273839e695aa
Danial <danialzainul@hotmail.com>
2016-05-11 00:26:52 +0800

    complete user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,4 +24,38 @@ describe "User pages" do
 		it { should have_title(user.name) }
 	end
 
+	# Testing user signup page
+	describe "signup page" do
+		before { visit signup_path }
+
+		it { should have_content('Signup') }
+		it { should have_title(full_title('Signup')) }
+	end
+
+	describe "signup" do
+		before { visit signup_path }
+
+		let(:submit) { "Create my account" } 
+
+		describe "with INVALID information" do
+			it "should NOT create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with VALID information" do
+			before do
+				fill_in "Name", 		with: "Example User"
+				fill_in "Email", 		with: "user@example.com"
+				fill_in "Password", with: "foobar"
+				fill_in "Confirmation", with: "foobar"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+		end
+	end
+
 end
