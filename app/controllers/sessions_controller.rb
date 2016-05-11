c741af6dfc74af3f84da05cc7b80fbcba962212e
Danial <danialzainul@hotmail.com>
2016-05-11 17:46:22 +0800

    WIP user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+	  user = User.find_by(email: params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+	  	sign_in user
+	    flash[:success] = 'Successfully logged in'
+	    redirect_to user_path(user)
+	  else
+	  	flash.now[:error] = 'Invalid email/password combination'
+	  	render 'new'
+	  end
 	end
 
 	def destroy
