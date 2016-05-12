f855bce693d43ba819769c00bf30c691ed6521d8
Danial <danialzainul@hotmail.com>
2016-05-12 17:15:24 +0800

    added users show, and fixed a few other tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,6 +5,24 @@ describe "User pages" do
 	
 	subject { page } # makes page the default subject of the test example
 
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
+    end
+  end
+
 	# Testing user profile page
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
@@ -46,14 +64,14 @@ describe "User pages" do
 			end
 		end
 
-  	# describe "after saving the user" do
-   #    before { click_button submit }
-   #    let(:user) { User.find_by_email('user@example.com') }
+    # describe "after saving the user" do
+    #   before { click_button submit }
+    #   let(:user) { User.find_by(email: 'user@example.com') }
 
-		 #  it { should have_title(user.name) }
-		 #  it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-   #    it { should have_link('Sign out') }
-   #  end
+    #   it { should have_link('Sign out') }
+    #   it { should have_title(user.name) }
+    #   it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    # end
 
 		describe "after submission" do
 			before { click_button submit }
@@ -71,36 +89,38 @@ describe "User pages" do
       visit edit_user_path(user)
     end
 
-    # describe "page" do
-    #   it { should have_content("Update your profile") }
-    #   it { should have_title("Edit user") }
-    #   it { should have_link('change', href: 'http://gravatar.com/emails') }
-    # end
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
 
-    # describe "EDIT with invalid information" do
-    #   before { click_button "Save changes" }
+    describe "EDIT with invalid information" do
+      before { click_button "Save changes" }
 
-    #   it { should have_content('error') }
-    # end
+      it { should have_content('error') }
+    end
 
-    # describe "EDIT with valid information" do
-    #   let(:new_name)  { "New Name" }
-    #   let(:new_email) { "new@example.com" }
-    #   before do
-    #     fill_in "Name",             with: new_name
-    #     fill_in "Email",            with: new_email
-    #     fill_in "Password",         with: user.password
-    #     fill_in "Confirm Password", with: user.password
-    #     click_button "Save changes"
-    #   end
+    describe "EDIT with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
 
-    #   it { should have_selector('title', text: new_name) }
-    #   it { should have_selector('div.alert.alert-success') }
-    #   it { should have_link('Sign out', href: signout_path) }
-    #   specify { user.reload.name.should  == new_name }
-    #   specify { user.reload.email.should == new_email }
-    # end
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
 
   end # --- close "edit" ---
+
+
 
 end
